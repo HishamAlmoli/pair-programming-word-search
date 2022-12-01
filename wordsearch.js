e4287a73c50ddad232d36712267ae51714d16815
HishamAlmoli <halmoli@my.centennialcollege.ca>
2022-11-30 19:17:16 -0500

    Done

--- wordsearch.js
+++ wordsearch.js
@@ -1,7 +1,13 @@
 const wordSearch = (letters, word) => { 
+    let counter = 0;
+    const verJoin = letters.map(item => item[counter]).join('');
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
+    }
+    for (item of verJoin) {
+        ++counter;
+        if (!item.includes(word)) return false
     }
 }
 
